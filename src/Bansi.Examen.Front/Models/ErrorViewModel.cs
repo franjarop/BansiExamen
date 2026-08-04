04ac285fc677cb48bc68e093eb6082b8d3b3f72c
franjarop <franjarop.sistemas@gmail.com>
2026-08-03 18:45:23 -0600

    chore: creación de esqueleto para el armado de la solución

---
+++ src/Bansi.Examen.Front/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bansi.Examen.Front.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
